3c3cd08c74049e504d121ebb125374f87dfd2e07
whw <>
2017-08-17 16:55:38 +0800

    ok=====================

--- dubbo_project/src/main/java/com/jiuyuvip/controller/GoodsController.java
+++ dubbo_project/src/main/java/com/jiuyuvip/controller/GoodsController.java
@@ -9,10 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  *  GoodsController
